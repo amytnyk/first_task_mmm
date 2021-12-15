976b773570f7e120ae83c6265012d2eb84a79c15
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2021-12-15 11:55:39 +0200

    improved header

--- todo.py
+++ todo.py
@@ -9,10 +9,29 @@ def write_to_csv(dct):
     :param dct:
     :return:
     """
+    num_tasks = find_busiest(dct)
     header = ["DATE"]
+    for i in range(num_tasks):
+        header.append(f"TASK {i}")
     with open("task_list.csv", "w", encoding="utf-8") as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(header)
         for the_date, tasks in dct.items():
-            row = the_date+tasks
+            row = [the_date] + tasks
             writer.writerow(row)
+
+
+def find_busiest(dct):
+    max_tasks = 0
+    for key, value in dct.items():
+        num_tasks = len(value)
+        if num_tasks > max_tasks:
+            max_tasks = num_tasks
+    return max_tasks
+
+
+if __name__ == "__main__":
+    dct = {}
+    d = date(2002, 12, 31)
+    dct[d] = ["sth", "sth1", "sth2"]
+    write_to_csv(dct)
