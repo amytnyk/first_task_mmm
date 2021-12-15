b57ab97ab30c5d4b2a8354c035e7e5784459635b
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2021-12-15 11:46:30 +0200

    added csv_writer

--- todo.py
+++ todo.py
@@ -1,1 +1,18 @@
 # main script
+from datetime import date
+import csv
+
+
+def write_to_csv(dct):
+    """
+    the function writes the tasks into csv file
+    :param dct:
+    :return:
+    """
+    header = ["DATE"]
+    with open("task_list.csv", "w", encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(header)
+        for the_date, tasks in dct.items():
+            row = the_date+tasks
+            writer.writerow(row)
