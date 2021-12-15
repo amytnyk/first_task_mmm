1cae7ed5965731bbe212275e43c5edfed43684f6
MatthewPr12 <matvii.prytula@ucu.edu.ua>
2021-12-15 12:19:55 +0200

    added search by name func to done func

--- todo.py
+++ todo.py
@@ -1,6 +1,7 @@
 # main script
 from datetime import date
 import csv
+import re
 
 
 def write_to_csv(dct):
@@ -28,6 +29,36 @@ def find_busiest(dct):
         if num_tasks > max_tasks:
             max_tasks = num_tasks
     return max_tasks
+
+
+def search_by_first_word(dct, first_word):
+    pattern = f"^{first_word}"
+    for the_date, tasks in dct.items():
+        for the_task in tasks:
+            if re.search(pattern, the_task):
+                dct[the_date].remove(the_task)
+    return dct
+
+
+def search_by_date_priority(dct, wanted_date, priority):
+    the_year = wanted_date.split("-")[0]
+    the_month = wanted_date.split("-")[1]
+    the_day = wanted_date.split("-")[2]
+    for dates, tasks in dct.items():
+        if dates==
+
+
+def mark_as_done(dct):
+    way = input("Type 1 if you want to find task by the date and priority"
+                "Type 2 if you want to find task by the first word")
+    if way == 2:
+        first_word = input("Type the word you want to find the task by")
+        search_by_first_word(dct, first_word)
+    elif way == 1:
+        wanted_date = input("Type the date in format YYYY-MM-DD")
+        priority = input("Type the priority of the task")
+
+
 
 
 if __name__ == "__main__":
